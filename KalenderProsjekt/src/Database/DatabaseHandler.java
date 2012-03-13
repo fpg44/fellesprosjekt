55796dd14d3c325002a2dbcac87613fc59e0147b
Robin Tordly <robin.tordly@gmail.com>
2012-03-13 13:57:15 +0100

    Laget skallet til Server og koblet til databasehandler

--- KalenderProsjekt/src/Database/DatabaseHandler.java
+++ KalenderProsjekt/src/Database/DatabaseHandler.java
@@ -5,6 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import models.Event;
+
 public class DatabaseHandler {
 
 	private Connection con = null;
@@ -50,5 +52,9 @@ public class DatabaseHandler {
 		}catch( SQLException e){
 			System.out.println("Failed to close database connection");
 		}
+	}
+	
+	public void insertIntoDatabase(Event event){
+		
 	}
 }

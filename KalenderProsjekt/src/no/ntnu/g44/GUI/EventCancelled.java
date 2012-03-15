05465597a1c526cde7e05c604d085b50d54ce2b0
Jeppe Eriksen <jeppeer@gmail.com>
2012-03-15 11:14:05 +0100

    Working with Event cancelled

--- KalenderProsjekt/src/no/ntnu/g44/GUI/EventCancelled.java
+++ KalenderProsjekt/src/no/ntnu/g44/GUI/EventCancelled.java
@@ -1,5 +1,49 @@
 package no.ntnu.g44.gui;
 
-public class EventCancelled {
+import java.awt.Event;
+import java.sql.Date;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+import no.ntnu.g44.models.Person;
+import no.ntnu.g44.models.Room;
+
+public class EventCancelled extends JPanel{
+	
+	private Event event;
+	
+	private Person eventOwner;
+	private Date startTime, endTime;
+	private Room room;
+	private String cusomtLocation;
+	
+	private JLabel staticPersonNameLabel, saticStartTimeLabel, staticEndTimeLabel, staticLocationLabel;
+	private JLabel personNameLabel, startTimeLabel, endTimeLabel, locationLabel; 
+	private JTextArea textArea;
+	
+	public EventCancelled(Event event) {
+		this.event = event;
+		init();
+		
+		/*
+		this.eventOwner = eventOwner;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.room = room;
+		this.cusomtLocation = customLocation;
+		*/
+	}
+	
+	private void init() {
+		String eventTitle = ((Event) event).getEventTitle();
+	}
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame();
+		Person person = new Person("Jeppe Eriksen", "jeppeer@gmail.com");
+		Date startDate = 
+	}
 }

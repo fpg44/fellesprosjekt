4c5700253ac6178652a2d73c7bca257198ac693b
= <gwethir@gmail.com>
2012-03-15 10:19:02 +0100

    added main class

--- KalenderProsjekt/src/no/ntnu/g44/controllers/Main.java
+++ KalenderProsjekt/src/no/ntnu/g44/controllers/Main.java
@@ -1,13 +1,14 @@
 package no.ntnu.g44.controllers;
 
+import no.ntnu.g44.gui.MainFrame;
+
 public class Main {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		new MainFrame();
 	}
 
 }

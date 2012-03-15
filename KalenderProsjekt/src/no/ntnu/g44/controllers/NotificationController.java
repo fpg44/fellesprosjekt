febee7914fb26822518c0167ee33f068c362bc77
Jeppe Eriksen <jeppeer@gmail.com>
2012-03-15 15:41:16 +0100

    Small changes to the notification classes

--- KalenderProsjekt/src/no/ntnu/g44/controllers/NotificationController.java
+++ KalenderProsjekt/src/no/ntnu/g44/controllers/NotificationController.java
@@ -4,27 +4,29 @@ import java.util.ArrayList;
 
 import no.ntnu.g44.models.Notification;
 import no.ntnu.g44.models.NotificationType;
-
+/**
+ * 
+ * @author JeppeE
+ *
+ */
 public class NotificationController {
 	
 	ArrayList unseenNotifications;
 	
 	public NotificationController() {
 		unseenNotifications = new ArrayList();
-
-//		unseenNotifications.add(new Notification(12, 123123, Message, type))
-		unseenNotifications.add(new Notification(123, 123, NotificationType.CANCELLED));
 		
-		/*
-		unseenNotifications.add(new Notification(NotificationType.EVENT_CANCELLED));
-		unseenNotifications.add(new Notification(NotificationType.EVENT_INVITATION));
-		unseenNotifications.add(new Notification(NotificationType.EVENT_INVITATION));
-		unseenNotifications.add(new Notification(NotificationType.EVENT_INVITATION));
-		unseenNotifications.add(new Notification(NotificationType.EVENT_CANCELLED));
-		unseenNotifications.add(new Notification(NotificationType.EVENT_CANCELLED));
-		*/
+		//Hard coded notifications for testing purposes
+		unseenNotifications.add(new Notification(123, 123, NotificationType.CANCELLED));
+		unseenNotifications.add(new Notification(123, 123, NotificationType.INVITATION));
+		unseenNotifications.add(new Notification(123, 123, NotificationType.DECLINER));
+		unseenNotifications.add(new Notification(123, 123, NotificationType.CHANGED));
 	}
 	
+	/**
+	 * 
+	 * @return An array with all the notifications of a user.
+	 */
 	public ArrayList<Notification> getUnseenNotifications() {
 		return unseenNotifications;
 	}

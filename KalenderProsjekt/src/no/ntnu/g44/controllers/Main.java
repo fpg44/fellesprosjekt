4792eb9e34f4263e5f06be3254d771001f775d17
= <gwethir@gmail.com>
2012-03-15 10:21:50 +0100

    added onLogin method in main

--- KalenderProsjekt/src/no/ntnu/g44/controllers/Main.java
+++ KalenderProsjekt/src/no/ntnu/g44/controllers/Main.java
@@ -1,14 +1,21 @@
 package no.ntnu.g44.controllers;
 
 import no.ntnu.g44.gui.MainFrame;
+import no.ntnu.g44.models.Project;
 
 public class Main {
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) {		
+		//TODO: Create new login here
+		//MainFrame should be created in Project constructor
 		new MainFrame();
+	}
+	
+	public static void onLogin(String username){
+		new Project();
 	}
 
 }

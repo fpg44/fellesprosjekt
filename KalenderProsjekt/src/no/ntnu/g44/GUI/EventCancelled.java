cb6eed319308aa748327e72a11b4d9ece1238af7
Jeppe Eriksen <jeppeer@gmail.com>
2012-03-15 11:38:16 +0100

    Made some changes to the Event-class. Still working on Event Cancelled.

--- KalenderProsjekt/src/no/ntnu/g44/GUI/EventCancelled.java
+++ KalenderProsjekt/src/no/ntnu/g44/GUI/EventCancelled.java
@@ -1,13 +1,14 @@
 package no.ntnu.g44.gui;
 
-import java.awt.Event;
-import java.sql.Date;
+
+import java.util.Date;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+import no.ntnu.g44.models.Event;
 import no.ntnu.g44.models.Person;
 import no.ntnu.g44.models.Room;
 
@@ -18,7 +19,7 @@ public class EventCancelled extends JPanel{
 	private Person eventOwner;
 	private Date startTime, endTime;
 	private Room room;
-	private String cusomtLocation;
+	private String customLocation;
 	
 	private JLabel staticPersonNameLabel, saticStartTimeLabel, staticEndTimeLabel, staticLocationLabel;
 	private JLabel personNameLabel, startTimeLabel, endTimeLabel, locationLabel; 
@@ -26,24 +27,25 @@ public class EventCancelled extends JPanel{
 	
 	public EventCancelled(Event event) {
 		this.event = event;
-		init();
 		
-		/*
-		this.eventOwner = eventOwner;
-		this.startTime = startTime;
-		this.endTime = endTime;
-		this.room = room;
-		this.cusomtLocation = customLocation;
-		*/
+		init();
 	}
 	
 	private void init() {
-		String eventTitle = ((Event) event).getEventTitle();
+		this.eventOwner = event.getEventOwner();
+		this.startTime = event.getEventStartTime();
+		this.endTime = event.getEventEndTime();
+		this.room = event.getRoom();
+		this.customLocation = event.getLocation();
 	}
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
 		Person person = new Person("Jeppe Eriksen", "jeppeer@gmail.com");
-		Date startDate = 
+		Event newEvent = new Event("TestEvent", person, null, new Date(2012,3,15,11,15),
+				new Date(2012,3,15,13,6), null,null);
+		EventCancelled ec = new EventCancelled(newEvent);
+		
+		frame.getContentPane();
 	}
 }

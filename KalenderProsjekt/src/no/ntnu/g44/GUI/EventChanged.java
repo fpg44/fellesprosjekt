88860c576ea1fc7269a85134150bcf5ebeafd840
haiirokage <andahlin@gmail.com>
2012-03-19 10:00:45 +0100

    k

---
+++ KalenderProsjekt/src/no/ntnu/g44/GUI/EventChanged.java
@@ -0,0 +1,13 @@
+package no.ntnu.g44.gui;
+
+public class EventChanged {
+	public EventChanged(){
+		
+	}
+	public static void makeEventChangedPanel(){
+		EventChanged panel = new EventChanged();
+	}
+	public static void main(String[] lool){
+		makeEventChangedPanel();
+	}
+}

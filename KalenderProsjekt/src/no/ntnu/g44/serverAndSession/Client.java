672beb9a892acff787fdadc93778c38d4ce140a5
= <gwethir@gmail.com>
2012-03-20 13:59:55 +0100

    now spreads messages!!!

--- KalenderProsjekt/src/no/ntnu/g44/serverAndSession/Client.java
+++ KalenderProsjekt/src/no/ntnu/g44/serverAndSession/Client.java
@@ -27,9 +27,20 @@ public class Client {
 		try {
 			connection.connect(InetAddress.getByName(serverIP), serverPort);
 			connection.send("halla!!!");
+			recieveLoop();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-	}	
+	}
+	
+	public void recieveLoop(){
+		while(true){
+			try {
+				System.out.println(connection.receive());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
 }

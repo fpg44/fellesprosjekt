921ea2af737f95d0b50171ab9c069ee2fd003689
Robin Tordly <robin.tordly@gmail.com>
2012-03-24 13:08:58 +0100

    .

--- KalenderProsjekt/src/no/ntnu/g44/models/Notification.java
+++ KalenderProsjekt/src/no/ntnu/g44/models/Notification.java
@@ -47,10 +47,9 @@ public class Notification {
 	}
 
 	public Notification(int eventId, NotificationType type){
-		this(Main.currentProject.generateID(), eventId, type);
+//		this(Main.currentProject.generateID(), eventId, type);
 		System.out.println("IS THIS NULL? " + eventId);
 		System.out.println("IS THIS NULL? " + type.toString());
-		System.out.println("IS THIS NULL? " + this.eventID);
 	}
 
 	public int getNotificationID() {
